aaaf6acfdee61e1d19cde2dd0d0e9f1dff748aa7
amitshob <amitshob@gmail.com>
2015-09-16 17:08:04 -0700

    Update specs.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,16 @@
-describe('Player', function() {
-  it("returns the player's mark", function() {
-    var testPlayer = new Player("X");
-    expect(testPlayer.mark()).to.equal("X");
-  });
-});
+// describe('Player', function() {
+//   it("returns the player's mark", function() {
+//     var testPlayer = new Player("X");
+//     expect(testPlayer.mark()).to.equal("X");
+//   });
+// });
 
-describe('Space', function() {
-  it("returns the player's mark", function() {
-    var testSpace = new Space(1,2);
-    expect(testSpace.x-coordinate()).to.equal(1);
-  });
-});
+// describe('Space', function() {
+//   it("returns the player's mark", function() {
+//     var testSpace = new Space(1,2);
+//     expect(testSpace.x-coordinate()).to.equal(1);
+//   });
+// });
 
 // describe('Space', function() {
 //   it("returns the player's mark", function() {
